03430a572df95f8ee037b746813a9e2b6701d783
Incognito <stephan.z@mail.ru>
2025-12-04 14:24:22 +0300

    pool_pre_ping=True

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ password = environ.get('DATABASE_PASSWORD')
 host = environ.get('DATABASE_HOST')
 name = environ.get('DATABASE_NAME')
 
-async_engine = create_async_engine(f'postgresql+asyncpg://{username}:{password}@{host}/{name}')
+async_engine = create_async_engine(f'postgresql+asyncpg://{username}:{password}@{host}/{name}', pool_pre_ping=True,
+                                   pool_recycle=3600, pool_size=2, max_overflow=4)
 
 AsyncSession = async_sessionmaker(async_engine)
