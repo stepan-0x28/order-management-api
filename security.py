23a2aa98d915bbe1cf320194cc09b0a0a816fa57
Stepan <stephan.z@mail.ru>
2026-07-22 00:30:34 +0300

    refactoring and minor changes (security; removing old)

--- security.py
+++
@@ -1,8 +0,0 @@
-from fastapi.security import OAuth2PasswordBearer
-from os import environ
-
-from services.token import Token as TokenService
-
-oauth2_scheme = OAuth2PasswordBearer('token')
-
-token_service = TokenService(environ.get('ENCRYPTION_KEY'))
