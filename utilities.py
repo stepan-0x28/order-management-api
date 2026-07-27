e463b799fda509e0266189afd4c6e9ab1df1bc19
Stepan <stephan.z@mail.ru>
2026-07-27 06:45:09 +0300

    refactoring and minor changes (removing old)

--- utilities.py
+++
@@ -1,9 +0,0 @@
-from typing import Any, Dict
-
-
-def remove_keys(dictionary: Dict[str, Any], *keys: str) -> Dict[str, Any]:
-    for key in keys:
-        if key in dictionary:
-            del dictionary[key]
-
-    return dictionary
