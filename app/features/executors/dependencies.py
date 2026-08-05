094c171b6afb824cf862f92b65fbb346ec9328de
Stepan <stephan.z@mail.ru>
2026-08-05 05:56:36 +0300

    executors correction

--- app/features/executors/dependencies.py
+++ app/features/executors/dependencies.py
@@ -1,11 +1,17 @@
 from fastapi import Depends
 from typing import Annotated
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.dependencies import get_session
+from app.core.dependencies import get_db_session
+from app.core.database.session import SessionWrapper
 
+from app.features.executors.repository import ExecutorRepository
 from app.features.executors.service import ExecutorService
 
 
-def get_executor_service(session: Annotated[AsyncSession, Depends(get_session)]) -> ExecutorService:
-    return ExecutorService(session)
+def get_executor_repository(session: Annotated[SessionWrapper, Depends(get_db_session)]) -> ExecutorRepository:
+    return ExecutorRepository(session)
+
+
+def get_executor_service(executor_repository: Annotated[ExecutorRepository, Depends(get_executor_repository)]
+                         ) -> ExecutorService:
+    return ExecutorService(executor_repository)
--- app/features/executors/service.py
+++ app/features/executors/service.py
@@ -1,13 +1,12 @@
 from typing import Sequence
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.features.executors.repository import ExecutorRepository
 from app.features.users.model import User
 
 
 class ExecutorService:
-    def __init__(self, session: AsyncSession):
-        self.__executor_repository = ExecutorRepository(session)
+    def __init__(self, executor_repository: ExecutorRepository):
+        self.__executor_repository = executor_repository
 
     async def get_all(self) -> Sequence[User]:
         return await self.__executor_repository.get_all()
