fa117861e2777d4309f7789299ff14d7c94f127c
Stepan <stephan.z@mail.ru>
2026-08-06 05:41:38 +0300

    correction

--- app/features/orders/repository.py
+++ app/features/orders/repository.py
@@ -10,20 +10,23 @@ from app.features.orders.model import Order
 
 class OrderRepository(BaseRepository):
     @staticmethod
-    def __build_base_orders_select(user_column: InstrumentedAttribute, user_id: int) -> Select:
+    def __build_base_user_orders_select(user_column: InstrumentedAttribute, user_id: int) -> Select:
         return select(Order).where(operators.eq(user_column, user_id))
 
-    def __build_orders_select(self, user_column: InstrumentedAttribute, user_id: int, include_deleted: bool) -> Select:
-        statement = self.__build_base_orders_select(user_column, user_id)
+    def __build_user_orders_select(self,
+                                   user_column: InstrumentedAttribute,
+                                   user_id: int,
+                                   include_deleted: bool) -> Select:
+        statement = self.__build_base_user_orders_select(user_column, user_id)
 
         if not include_deleted:
             statement = statement.where(Order.is_deleted == false())
 
         return statement
 
-    def __build_order_select(self, user_column: InstrumentedAttribute, user_id: int, order_id: int) -> Select:
+    def __build_user_order_select(self, user_column: InstrumentedAttribute, user_id: int, order_id: int) -> Select:
         statement = (
-            self.__build_base_orders_select(user_column, user_id)
+            self.__build_base_user_orders_select(user_column, user_id)
             .where(Order.id == order_id)
             .where(Order.is_deleted == false())
         )
@@ -31,13 +34,13 @@ class OrderRepository(BaseRepository):
         return statement
 
     async def get_customer_orders(self, customer_id: int, include_deleted: bool) -> Sequence[Order]:
-        return await self._get_all(self.__build_orders_select(Order.customer_id, customer_id, include_deleted))
+        return await self._get_all(self.__build_user_orders_select(Order.customer_id, customer_id, include_deleted))
 
     async def get_executor_orders(self, executor_id: int, include_deleted: bool) -> Sequence[Order]:
-        return await self._get_all(self.__build_orders_select(Order.executor_id, executor_id, include_deleted))
+        return await self._get_all(self.__build_user_orders_select(Order.executor_id, executor_id, include_deleted))
 
     async def get_customer_order(self, customer_id: int, order_id: int) -> Optional[Order]:
-        return await self._get_one(self.__build_order_select(Order.customer_id, customer_id, order_id))
+        return await self._get_one(self.__build_user_order_select(Order.customer_id, customer_id, order_id))
 
     async def get_executor_order(self, executor_id: int, order_id: int) -> Optional[Order]:
-        return await self._get_one(self.__build_order_select(Order.executor_id, executor_id, order_id))
+        return await self._get_one(self.__build_user_order_select(Order.executor_id, executor_id, order_id))
